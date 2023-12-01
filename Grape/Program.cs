52d2bb7a027e1df57c97c09c7dec4fb23c90f512
Shakirah-Jade <shakirahhadijjah5@gmail.com>
2023-12-01 12:33:46 +0300

    First Commit

---
+++ Grape/Program.cs
@@ -0,0 +1,30 @@
+﻿using CameraInteraction;
+using Cherry;
+using System;
+
+namespace Grape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           double bmr = BMRCalculator.CalculateBMR("male", 70, 175.0, 30);
+
+          
+            /* string activityLevel = "moderately active";
+             double dailycaloricintake = BMRCalculator.CalculateDailyCaloricIntake(bmr, activityLevel); // getting the dailycaloric intake
+             Console.WriteLine($"The DailyCaloricIntake for a {gender} with age {age} and the {bmr} calories per day is: {dailycaloricintake}");
+
+             double dailyexpenditure = BMRCalculator.CalculateTotalDailyExpenditure(bmr, dailycaloricintake);
+             double depletion = BMRCalculator.CalculateGlucoseDepletion(dailyexpenditure);
+             double bodyintake = BMRCalculator.CalculateBodyIntakeRate(depletion);
+             double foodcomposition = BMRCalculator.CalculateFoodGlucoseComposition(40, bodyintake);*/
+
+
+            // interaction with the camera.
+            /*Interaction cameraInteraction = new Interaction();
+            cameraInteraction.StartCamera();
+*/
+        }
+    }
+}
