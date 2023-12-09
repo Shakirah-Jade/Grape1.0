cb21d9cc8cd407ee44254c3340ab43eb8c338874
Shakirah-Jade <shakirahhadijjah5@gmail.com>
2023-12-09 18:55:02 +0300

    With Module Three

--- Grape/Program.cs
+++ Grape/Program.cs
@@ -1,21 +1,19 @@
-﻿using CameraInteraction;
-using Cherry;
+﻿using Cherry;
 using System;
 using System.Collections.Generic;
+using static Cherry.HealthProTechLibrary;
 
 namespace Grape
 {
-    // Program.cs
     class Program
     {
         static void Main()
         {
             // Instantiate the HealthCalculatorModule
             var healthCalculator = new HealthProTechLibrary.HealthCalculatorModule();
-            var gender = new HealthProTechLibrary.Gender();
 
             // Calculate BMR
-            double bmr = healthCalculator.CalculateBMR(weight: 70, height: 170, age: 25,);
+            double bmr = healthCalculator.CalculateBMR(weight: 70, height: 170, age: 25, Gender.Male);
             Console.WriteLine($"Basal Metabolic Rate (BMR): {bmr} calories");
 
             // Calculate TDEE
@@ -28,7 +26,7 @@ namespace Grape
 
             // Record Glucose Level for a User
             string userId = "User123";
-            double glucoseLevel = 120;
+            double glucoseLevel = 7; //in mmol/L
             healthCalculator.RecordGlucoseLevel(userId, glucoseLevel);
 
             // Get Stored Glucose Levels for a User
@@ -40,7 +38,47 @@ namespace Grape
             {
                 Console.WriteLine(level);
             }
+
+
+
+/*
+            // Create an instance of the ImageProcessingModule
+            var imageProcessor = new HealthProTechLibrary.ImageProcessingModule();
+
+            // Start the camera and capture an image
+            imageProcessor.StartCamera();*/
+
+            string csvFilePath = "C:\\Users\\User\\Desktop\\sem1\\MiniProject\\food.csv";
+
+            // Create an instance of the DietaryAnalyzerModule
+             var dietaryAnalyzerModule = new HealthProTechLibrary.DietaryAnalyzerModule(healthCalculator);
+
+            // Load food composition data from the CSV file
+            var foodCompositionData = dietaryAnalyzerModule.LoadFoodCompositionData(csvFilePath);
+
+            // Print the loaded data to the terminal
+            /*PrintFoodCompositionData(foodCompositionData);*/
+            dietaryAnalyzerModule.RecommendFoodsBasedOnCaloricIntake(csvFilePath, userId,UserGoal.WeightLoss);
         }
+        
+        static void PrintFoodCompositionData(List<FoodCompositionData> foodCompositionData)
+        {
+            Console.WriteLine("Food Composition Data:");
+            foreach (var foodData in foodCompositionData)
+            {
+                Console.WriteLine($"FoodName: {foodData.FoodName}");
+                Console.WriteLine($"Country: {foodData.Country}");
+                Console.WriteLine($"Protein: {foodData.Protein}");
+                Console.WriteLine($"Carbohydrate: {foodData.Carbohydrate}");
+                Console.WriteLine($"Lipids: {foodData.Lipids}");
+                Console.WriteLine($"Fibre: {foodData.Fibre}");
+                Console.WriteLine($"Energy: {foodData.Energy}");
+
+                // Add additional properties as needed
+                Console.WriteLine("------------------------");
+            }
+        }
+
     }
 
 }
